d5982899db00db2e30d68e440648befbd72737d4
Alastair Taft <ali.c.taft@gmail.com>
2019-03-21 22:46:29 +1100

    Adds a ref to hook into the original instance method.

--- src/hook.js
+++ src/hook.js
@@ -72,7 +72,10 @@ export default function hook(WrappedComponent) {
     }
 
     render() {
-      return <WrappedComponent generateTestHook={this.generateTestHook} {...this.props} />;
+      return <WrappedComponent
+        ref={c => this._instance = c}
+        generateTestHook={this.generateTestHook} {...this.props}
+      />;
     }
   };
 
